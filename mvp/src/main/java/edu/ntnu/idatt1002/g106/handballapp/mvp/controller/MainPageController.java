9ec8e4f14fe86751aa401a7f0521500439633175
Tomas Beranek <tomaber@stud.ntnu.no>
2022-03-23 23:19:26 +0100

    refactor: small changes to logoutButton

--- mvp/src/main/java/edu/ntnu/idatt1002/g106/handballapp/mvp/controller/MainPageController.java
+++ mvp/src/main/java/edu/ntnu/idatt1002/g106/handballapp/mvp/controller/MainPageController.java
@@ -1,18 +1,37 @@
 package edu.ntnu.idatt1002.g106.handballapp.mvp.controller;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;;
-import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainPageController implements Initializable {
 
+    @FXML
+     private AnchorPane scenePane;
+    Stage stage;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void logout(){
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are about to logout");
+        alert.setContentText("Do you want to save before exiting");
+
+        if (alert.showAndWait().get() == ButtonType.OK){
+            stage = (Stage) scenePane.getScene().getWindow();
+            System.out.println("You successfully logged out!");
+            stage.close();
+        }
     }
 }
