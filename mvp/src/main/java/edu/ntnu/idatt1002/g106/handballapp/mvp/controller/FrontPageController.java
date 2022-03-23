806faa88a03617f0488fd5daccf8b0d5c5af51c2
Tomas Beranek <tomaber@stud.ntnu.no>
2022-03-23 17:26:03 +0100

    refactor: adding to see if it works

--- mvp/src/main/java/edu/ntnu/idatt1002/g106/handballapp/mvp/controller/FrontPageController.java
+++ mvp/src/main/java/edu/ntnu/idatt1002/g106/handballapp/mvp/controller/FrontPageController.java
@@ -1,4 +1,26 @@
 package edu.ntnu.idatt1002.g106.handballapp.mvp.controller;
 
-public class FrontPageController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FrontPageController implements Initializable{
+
+    @FXML private Button newTournamentButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+
+    @FXML
+    public void handleButtonClick(){
+        System.out.println("hei eirik");
+        newTournamentButton.setText("Eirik Er stygg");
+    }
+
 }
