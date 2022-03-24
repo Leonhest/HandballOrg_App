ba31d840709b36d076eb223f176275d627a28e78
Tomas Beranek <tomaber@stud.ntnu.no>
2022-03-24 12:53:12 +0100

    feat: added switchScene class that should work for all classes

---
+++ mvp/src/main/java/edu/ntnu/idatt1002/g106/handballapp/mvp/backend/SwitchScene.java
@@ -0,0 +1,31 @@
+package edu.ntnu.idatt1002.g106.handballapp.mvp.backend;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SwitchScene {
+
+    private static String location;
+
+    public SwitchScene(String location) {
+        SwitchScene.location = location;
+    }
+
+    public static void switchScene(String location, ActionEvent actionEvent) throws IOException {
+        Parent viewPage = FXMLLoader.load(SwitchScene.class.getResource("/edu/ntnu/idatt1002/g106/handballapp/mvp/view/" + getLocation() + ".fxml"));
+        Scene page = new Scene(viewPage);
+        Stage window = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(page);
+        window.show();
+    }
+
+    public static String getLocation() {
+        return location;
+    }
+}
