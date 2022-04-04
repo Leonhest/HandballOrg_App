ad9b6bec7d6cb9800689831591b08098d17aff1c
Tomas Beranek <tomaber@stud.ntnu.no>
2022-04-04 10:16:54 +0200

    feat: created login page skeleton

---
+++ mvp/src/main/java/edu/ntnu/idatt1002/g106/handballapp/mvp/controller/LoginController.java
@@ -0,0 +1,24 @@
+package edu.ntnu.idatt1002.g106.handballapp.mvp.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+    public void RegisterLicence(ActionEvent event) {
+
+    }
+
+    public void ResetPssword(ActionEvent event) {
+    }
+}
