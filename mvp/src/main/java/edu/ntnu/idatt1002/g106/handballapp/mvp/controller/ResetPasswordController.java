9cf58db533a24d1e69192e7ba7d3ce306a42e2d0
Tomas Beranek <tomaber@stud.ntnu.no>
2022-04-04 10:37:56 +0200

    feat: created registrations class skeletons

--- mvp/src/main/java/edu/ntnu/idatt1002/g106/handballapp/mvp/controller/ResetPasswordController.java
+++ mvp/src/main/java/edu/ntnu/idatt1002/g106/handballapp/mvp/controller/ResetPasswordController.java
@@ -1,24 +1,20 @@
 package edu.ntnu.idatt1002.g106.handballapp.mvp.controller;
 
+import edu.ntnu.idatt1002.g106.handballapp.mvp.backend.SwitchScene;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginController implements Initializable {
-
-
+public class ResetPasswordController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
-
-    public void RegisterLicence(ActionEvent event) {
-
-    }
-
-    public void ResetPassword(ActionEvent event) {
+    public void toLoginPage(ActionEvent event) throws IOException {
+        SwitchScene.switchScene("Login", event);
     }
 }
