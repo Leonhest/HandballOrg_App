28ba7b49f12dbbdd2dc055413389097cd1004ffc
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-04-05 10:58:55 +0200

    feat: add Field and FieldRegister classes

---
+++ finalprod/src/main/java/edu/ntnu/idatt1002/g106/handballapp/finalprod/backend/Field.java
@@ -0,0 +1,98 @@
+package edu.ntnu.idatt1002.g106.handballapp.finalprod.backend;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.chrono.ChronoLocalDateTime;
+import java.util.Map;
+
+public class Field {
+    private int numField;
+    private Map<LocalDateTime, Boolean> availabilitySchedule;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Field(int numField, LocalDate startDate, LocalDate endDate) {
+        this.numField = numField;
+        generateTimeSchedule(startDate, endDate);
+        this.startDate = startDate;
+        this.endDate = endDate;
+
+        //Todo: exception handling
+    }
+
+//    public Iterable<Map> getAvailabilitySchedule(){
+//
+//    }
+
+    @Deprecated
+    /**
+     * This method sets the availability for a given time as false.
+     * @param localDateTime
+     */
+    public void setAvailabilityBasedOnTime(LocalDateTime localDateTime){
+        this.availabilitySchedule.replace(localDateTime, false);
+    }
+
+
+    @Deprecated
+    /**
+     * This method checks whether the date given falls within the available times of the tournament
+     * @param localDateTime Given date and time
+     * @return              The status on whether it is within the interval or not
+     */
+    public boolean isAvailableInGivenTimeInterval(LocalDateTime localDateTime){
+        if(((localDateTime.getHour() + (localDateTime.getMinute()/60)) % 1.5) != 0 && isWithinTournamentDays(localDateTime)){
+            return availabilitySchedule.get(localDateTime);
+        }
+        return false; //This method is unfinished and not in use.
+
+    }
+
+    @Deprecated
+    /**
+     * This method is to be used in checking that a given date falls within the tournament.
+     * @param localDateTime A given date
+     * @return              Status of whether the date falls within the tournament dates
+     */
+    public boolean isWithinTournamentDays(LocalDateTime localDateTime){
+        return localDateTime.isAfter(ChronoLocalDateTime.from(startDate)) ||
+                localDateTime.isEqual(ChronoLocalDateTime.from(startDate)) ||
+                localDateTime.isBefore(ChronoLocalDateTime.from(endDate)) ||
+                localDateTime.isEqual(ChronoLocalDateTime.from(endDate));
+    }
+
+    @Deprecated
+    /**
+     * This method generates a time schedule for when tournaments are set up manually.
+     * @param startDate
+     * @param endDate
+     */
+    private void generateTimeSchedule(LocalDate startDate, LocalDate endDate){
+        int hours = 0;
+        int minutes = 0;
+        LocalDate dateOfTournament = startDate;
+        while(dateOfTournament.isBefore(endDate.plusDays(1))){
+            for(int i = 1; i < 10; i++){
+                if((i*1.5) % 1 == 0){
+                    hours = (int) (9 + (i*1.5));
+                    minutes = 0;
+                }
+                else{
+                    hours = (int) (9 + Math.floor(i * 1.5));
+                    minutes = 30;
+                }
+                availabilitySchedule.put(LocalDateTime.of(dateOfTournament, LocalTime.of(hours, minutes)), true);
+            }
+            dateOfTournament = dateOfTournament.plusDays(1);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Field{" +
+                "numField=" + numField +
+                ", availabilitySchedule=" + availabilitySchedule +
+                '}';
+    }
+}
