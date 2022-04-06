f1b91173885c5660a1e4ac3a0f2c0c98f7038d45
Hans Magne Asheim <hmasheim@stud.ntnu.no>
2022-04-06 16:26:20 +0200

    perf: remove code duplicate

--- finalprod/src/main/java/edu/ntnu/idatt1002/g106/handballapp/finalprod/controller/RegionChoiceController.java
+++ finalprod/src/main/java/edu/ntnu/idatt1002/g106/handballapp/finalprod/controller/RegionChoiceController.java
@@ -3,6 +3,7 @@ package edu.ntnu.idatt1002.g106.handballapp.finalprod.controller;
 import edu.ntnu.idatt1002.g106.handballapp.finalprod.backend.SwitchScene;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 import java.net.URL;
@@ -15,26 +16,9 @@ public class RegionChoiceController implements Initializable {
 
     }
 
-
-    //todo: remove duplicate...
-
-    public void toSouthernRegion(ActionEvent event) throws IOException {
-        HandballApplication.setChosenRegion("SouthernRegion");
-        SwitchScene.switchScene("RegionController", event);
-    }
-
-    public void toEasternRegion(ActionEvent event) throws IOException {
-        HandballApplication.setChosenRegion("EasternRegion");
-        SwitchScene.switchScene("RegionController", event);
-    }
-
-    public void toNorthernRegion(ActionEvent event) throws IOException {
-        HandballApplication.setChosenRegion("NorthernRegion");
-        SwitchScene.switchScene("RegionController", event);
-    }
-
-    public void toWesternRegion(ActionEvent event) throws IOException {
-        HandballApplication.setChosenRegion("WesternRegion");
+    public void toRegionController(ActionEvent event) throws IOException {
+        Button selected = (Button) event.getSource();
+        HandballApplication.setChosenRegion(selected.getId());
         SwitchScene.switchScene("RegionController", event);
     }
 }
