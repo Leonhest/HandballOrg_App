ae9eac1198c7e8d1354fb40f3444fe494a6b5296
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-04-19 17:56:49 +0200

    fix: add exception handling for duplicate team name registration

--- finalprod/src/main/java/edu/ntnu/idatt1002/g106/handballapp/finalprod/controller/TeamRegistryController.java
+++ finalprod/src/main/java/edu/ntnu/idatt1002/g106/handballapp/finalprod/controller/TeamRegistryController.java
@@ -12,6 +12,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class TeamRegistryController implements Initializable {
@@ -98,8 +99,7 @@ public class TeamRegistryController implements Initializable {
                 HandballApplication.adminList.get(0).getTournamentRegister().getTournaments()
                         .get(HandballApplication.chosenTournament).getTeamRegister().addTeam(team);
                 feedbackText.setText("");
-                //TODO: REMOVE COMMENT FROM LINE BELOW
-                //resetInfo();
+                resetInfo();
             }
 
         }
@@ -121,11 +121,11 @@ public class TeamRegistryController implements Initializable {
      */
     @FXML
     public void resetInfo(){
-        teamNameTextFieldInput.setText("");
-        teamLeaderTextFieldInput.setText("");
-        regionTextFieldInput.setText("");
-        phoneNumTextFieldInput.setText("");
-        numPlayerInput.setValue("");
+        teamNameTextFieldInput.clear();
+        teamLeaderTextFieldInput.clear();
+        regionTextFieldInput.clear();
+        phoneNumTextFieldInput.clear();
+        numPlayerInput.setValue(String.valueOf(7));
     }
 
     /**
@@ -138,7 +138,8 @@ public class TeamRegistryController implements Initializable {
         if(regionTextFieldInput.getText().isEmpty() || regionTextFieldInput.getText().isBlank()) throw new IllegalArgumentException("*Region is invalid!*");
         HandballApplication.adminList.get(0).getTournamentRegister().getTournaments().forEach(tour -> tour.getTeamRegister().getListTeams()
                 .forEach(team -> {
-                    if (team.getTelephoneNum() == Integer.parseInt(phoneNumTextFieldInput.getText())) throw new IllegalArgumentException("*Phone number taken*");
+                    if(Objects.equals(team.getTeamName(), teamNameTextFieldInput.getText())) throw new IllegalArgumentException("Team is already registered!");
+                    else if (team.getTelephoneNum() == Integer.parseInt(phoneNumTextFieldInput.getText())) throw new IllegalArgumentException("*Phone number taken*");
                 }));
     }
 
