672c4a46fd4c7d7262da91b2cc2dada65fec1b71
Leonhest <leon.hesthaug@gmail.com>
2022-04-22 21:01:17 +0200

    fix: wrong merge method chosen

--- finalprod/src/main/java/edu/ntnu/idatt1002/g106/handballapp/finalprod/controller/MainPageController.java
+++ finalprod/src/main/java/edu/ntnu/idatt1002/g106/handballapp/finalprod/controller/MainPageController.java
@@ -43,6 +43,7 @@ public class MainPageController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         updateTableView();
+        trackTableClick();
     }
 
     /**
